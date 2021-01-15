e542077062b8dcf647e461e3ba7515df22fb380e
RSalgadoRivera <rabdos7@gmail.com>
2021-01-15 14:22:35 -0600

    docs agregado

---
+++ docs/precache-manifest.e47d520b66d7e98f41d40ea9c7d0786e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafad233b4d5cc292bad26cc5b134d68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69c02cb6bba98a93c0a9",
+    "url": "/static/css/main.0d049eb4.chunk.css"
+  },
+  {
+    "revision": "d81b9142e2414b83b57d",
+    "url": "/static/js/2.e0d95179.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.e0d95179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c02cb6bba98a93c0a9",
+    "url": "/static/js/main.3e821502.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
